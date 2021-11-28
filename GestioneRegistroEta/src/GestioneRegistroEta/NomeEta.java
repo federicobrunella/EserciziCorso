29df5bb43293a09be5c2af01280ab3c9d39bcbdd
federicobrunella <48298111+federicobrunella@users.noreply.github.com>
2021-11-28 21:59:31 +0100

    Funzionalità base

--- GestioneRegistroEta/src/GestioneRegistroEta/NomeEta.java
+++ GestioneRegistroEta/src/GestioneRegistroEta/NomeEta.java
@@ -2,8 +2,39 @@ package GestioneRegistroEta;
 
 public class NomeEta {
 
-	public NomeEta() {
-		// TODO Auto-generated constructor stub
+	private int eta;
+	private String nome;
+
+	public NomeEta(String nome, int eta) {
+		this.eta=eta;
+		this.nome=nome;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof NomeEta) {
+			NomeEta toConfr = (NomeEta) obj;
+			if(toConfr.getEta() == this.getEta() && toConfr.getNome().equals(this.getNome()))
+				return true;
+			else return false;
+		}
+		else
+			return false;
 	}
 
 }
--- GestioneRegistroEta/src/GestioneRegistroEta/RegistroEta.java
+++ GestioneRegistroEta/src/GestioneRegistroEta/RegistroEta.java
@@ -1,9 +1,33 @@
 package GestioneRegistroEta;
+import java.util.Vector;
 
 public class RegistroEta {
-
-	public RegistroEta() {
-		// TODO Auto-generated constructor stub
+	
+	private String nomeRegistro;
+	Vector <NomeEta> registro = new Vector<NomeEta>();
+	
+	public RegistroEta(String nomeRegistro) {
+		this.nomeRegistro=nomeRegistro;
 	}
-
+	
+	public void add(NomeEta toAdd) {
+		registro.add(toAdd);
+	}
+	
+	public void incrementaEta(NomeEta toIncrement) {
+		for(NomeEta item: registro) {
+			if(item.equals(toIncrement))
+				item.setEta(item.getEta()+1);
+		}
+	}
+	
+	public String toString() {
+		String output = "----- REGISTRO " + this.nomeRegistro + " -----";
+		
+		for(NomeEta item: registro) {
+			output += "Nome: " + item.getNome() + " -- Eta: " + item.getEta() + " \n";
+		}
+		
+		return output;
+	}
 }
